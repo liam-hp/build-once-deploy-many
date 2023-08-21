e2b9e9313f412df43375299838fb0a4642323550
Liam Patty <liamh.patty@gmail.com>
2023-08-21 18:32:08 +0200

    feat(cicd): working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { env } from './env.js'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <p> {env.REACT_APP_ENV} </p>
       </header>
     </div>
   );
